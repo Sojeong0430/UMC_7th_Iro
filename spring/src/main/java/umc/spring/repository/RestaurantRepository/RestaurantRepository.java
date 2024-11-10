58baf9dd133fddeb0984ed132472aa67fc5e7c6f
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2024-11-10 16:06:50 +0900

    Delete spring directory

--- spring/src/main/java/umc/spring/repository/RestaurantRepository/RestaurantRepository.java
+++
@@ -1,7 +0,0 @@
-package umc.spring.repository.RestaurantRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import umc.spring.domain.Restaurant;
-
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long>, RestaurantRepositoryCustom {
-}
--- spring/src/main/java/umc/spring/repository/RestaurantRepository/RestaurantRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package umc.spring.repository.RestaurantRepository;
-
-import umc.spring.domain.Restaurant;
-
-import java.util.List;
-
-public interface RestaurantRepositoryCustom {
-    List<Restaurant> dynamicQueryWithBooleanBuilder(String name, Float score);
-}
