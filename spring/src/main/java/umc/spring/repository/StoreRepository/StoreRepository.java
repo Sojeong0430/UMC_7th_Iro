593ee67a8ab930e1df8e9019e8aeddae8f32cbb4
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2024-11-10 15:56:58 +0900

    Delete spring directory

--- spring/src/main/java/umc/spring/repository/StoreRepository/StoreRepository.java
+++
@@ -1,9 +0,0 @@
-package umc.spring.repository.StoreRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import umc.spring.domain.store;
-
-import java.util.List;
-public interface StoreRepository extends JpaRepository<store, Long>, StoreRepositoryCustom {
-
-}
--- spring/src/main/java/umc/spring/repository/StoreRepository/StoreRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package umc.spring.repository.StoreRepository;
-import umc.spring.domain.store;
-import java.util.List;
-public interface StoreRepositoryCustom {
-
-    List<store> dynamicQueryWithBooleanBuilder(String name, Float score);
-
-}
