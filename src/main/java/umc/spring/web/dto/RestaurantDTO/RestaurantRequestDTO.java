b901aee240bd35f7d2e347fabdbfb95438d70f76
Sojeong0430 <dlthwjd1629@gmail.com>
2024-11-20 00:01:06 +0900

    feat : 가게에 리뷰 추가하기 API

--- src/main/java/umc/spring/web/dto/RestaurantDTO/RestaurantRequestDTO.java
+++ src/main/java/umc/spring/web/dto/RestaurantDTO/RestaurantRequestDTO.java
@@ -1,12 +1,15 @@
 package umc.spring.web.dto.RestaurantDTO;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import umc.spring.domain.Region;
 
 public class RestaurantRequestDTO {
     @Getter
     public static class AddRestaurantDTO{
+        @NotNull
         String name; //이름
+        @NotNull
         String address; //주소
     }
 }
